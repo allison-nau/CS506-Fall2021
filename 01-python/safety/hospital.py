789873236c3bf55353bd9104ad51a4e25292d2b3
allison-nau <allisonnnau@gmail.com>
2021-09-23 14:11:15 -0400

    AMENDED box representing hospital

--- 01-python/safety/hospital.py
+++ 01-python/safety/hospital.py
@@ -1,3 +1,20 @@
-def draw_hospital():
-    print("hospital not found")
-    return
+def draw_hospital(size=20):
+    """
+    Prints a hospital.
+    :param size: number of size across.
+    """
+    print("-"*size*2)
+    midpoint = round(size/2)
+    for i in range(size-2):
+        for j in range(size*2):
+            if j == 0 or j == (size*2-1) or (j == size and i < (midpoint+2) and i > (midpoint -2) and i != midpoint):
+                print("|", end="")
+            elif i == midpoint and j > (midpoint*2-4) and j < (midpoint*2+4):
+                print("-", end="")
+            else:
+                print(" ", end="")
+        print("")
+    print("-"*size*2)
+
+if __name__ == '__main__':
+    draw_hospital()
