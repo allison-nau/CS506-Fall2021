2e2cf496643c8679b1d614f605d750253d4f1b08
allison-nau <allisonnnau@gmail.com>
2021-10-07 15:30:41 -0400

    Create test file for matrix
    tox, whether in virtual environment or not, will not see that numpy is installed

---
+++ 02-library/tests/test_matrix.py
@@ -0,0 +1,24 @@
+import pytest
+
+from cs506 import matrix
+
+def test_matrix():
+    # TODO
+    # Initialize numpy array:
+    array_sizes_to_make = [[2, 2], [3, 3]]
+    # loop over array sizes to try:python
+    for size in array_sizes_to_make:
+        # Create array:
+        print(f"Dimension of array: {size}")
+        the_array = np.random.randint(-10, 10, size=(size[0], size[1]))
+        the_array = the_array.astype(float)
+        print("The array:")
+        print(the_array)
+        # Actual determinant using numpy:
+        test_answer = round(np.linalg.det(the_array), 4)
+        print(f"Answer using numpy to compare: {test_answer}")
+        # Try my code:
+        my_det = round(get_determinant(your_array=the_array), 4)
+        print(f"My answer: {my_det}")
+        # Do the two numbers match:
+        print(f"Is it a match? {test_answer == my_det}")
