5677185761e0f28c590a170fcd63a76497047def
allison-nau <allisonnnau@gmail.com>
2021-10-14 14:42:23 -0400

    Updated with all filters, by combining two df for either side of "or"

--- 07-pandas/readdf.py
+++ 07-pandas/readdf.py
@@ -4,36 +4,33 @@ import pandas as pd
 
 
 def process(path):
-    # TODO
     """
-
-    :param path:
-    :return:
-    Implement a function process() that given the path to a CSV file as input, select those
+    Given the path to a CSV file as input, selects those
     entries with a valid license (not NaN), room type of Private room, price less than or equal to 80, minimum nights
     less than or equal to 2, number of reviewers more than or equal to 500, or those with a valid license (not NaN),
     a room type of Entire home/apt, price less than or equal to 100, minimum nights less than or equal to 1,
     number of reviewers more than or equal to 100, and finally present the result sorted according to latitude and
     then longitude in an ascending order and with only columns of id, latitude, longitude, room_type,
     price and availability_365.
+    :param path: path to csv file to load in.
+    :return: processed pandas dataframe
     """
     # Read in dataframe (original size: (16478, 18)):
     df1 = pd.read_csv(path)
     # Drop License NA (new dim: (4135, 18)):
     df1.dropna(how="any", subset=["license"], inplace=True)
-    # Private room only (1360, 18):
-    df1 = df1[df1["room_type"] == "Private room"]
-    # Price <= 80 (510, 18)
-    df1 = df1[df1["price"] <= 80]
-    # Minimum nights <= 1 (196, 18)
-    df1 = df1[df1["minimum_nights"] <= 2]
-    # Number of reviewers >= 100 (66, 18)
-    df1 = df1[df1['number_of_reviews'] >= 100]
+    # Filter dataframe:
+    df2 = df1[((df1["room_type"] == "Private room") & (df1["price"] <= 80) & (df1["minimum_nights"]) <= 2 & (df1['number_of_reviews'] >= 500))]
+    df3 = df1[((df1["room_type"] == "Entire home/apt") & (df1["price"] <= 100) & (df1["minimum_nights"] <= 1) & (df1['number_of_reviews'] >= 100))]
+    # Merge the two dataframes:
+    df4 = pd.concat([df2, df3], axis=0)
+    # df1 = df1[(((df1["room_type"] == "Private room") & (df1["price"] <= 80) & (df1["minimum_nights"]) <= 2 & (df1['number_of_reviews'] >= 500)) or
+    #            ((df1["room_type"] == "Entire home/apt") & (df1["price"] <= 100) & (df1["minimum_nights"] <= 1) & (df1['number_of_reviews'] >= 100)))]
     # Subset columns:
-    df1 = df1[["id", "latitude", "longitude", "room_type", "price", "availability_365"]]
+    df4 = df4[["id", "latitude", "longitude", "room_type", "price", "availability_365"]]
     # Sort on latitude and longitude:
-    df1 = df1.sort_values(by=["latitude", "longitude"], ascending=True)
-    return df1
+    df4 = df4.sort_values(by=["latitude", "longitude"], ascending=True)
+    return df4
 
 
 df = process('listings.csv')
